042ea83d50f2dc7ce02ff863adef32049e27bb84
leonardo197 <leonardocosta197@gmail.com>
2018-06-07 14:33:39 +0100

    login ok

--- ProjetoPW/estudante.php
+++ ProjetoPW/estudante.php
@@ -55,23 +55,4 @@
 
 
    }
-   
-   $estudantes = array();
-   $e1 = new Estudante();
-   $e1->setNome("João");
-   $e1->setIdade(19);
-   $e1->setUsername("joao");
-   $e1->setPassword("123");
-   
-   $e2 = new Estudante();
-   $e2->setNome("Joana");
-   $e2->setIdade(20);
-   $e2->setUsername("joana");
-   $e2->setPassword("456");
-   
-   $estudantes[0] = $e1;
-   $estudantes[2] = $e2;
-   
-   
-   
 ?>
--- ProjetoPW/login.php
+++ ProjetoPW/login.php
@@ -5,26 +5,32 @@ require_once './estudante.php';
 if (session_status() == PHP_SESSION_NONE)
                 session_start();
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+
+if(isset($_POST["username"]))
+    $username = $_POST["username"];
+
+if(isset($_POST["password"]))
+    $password = $_POST["password"];
 
 $found = false;
 
-include './mysql/mysqlConnect.php';
-$sql = "SELECT * FROM utilizador where username = '" . $username . "' and password = '" . $password . "'";
-$result = $GLOBALS["db.connection"]->query($sql);
+if(isset($_POST["username"]) && isset($_POST["password"]))
+{
+    include './mysql/mysqlConnect.php';
+    $sql = "SELECT * FROM utilizador where username = '" . $username . "' and password = '" . $password . "'";
+    $result = $GLOBALS["db.connection"]->query($sql);
 
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $found = true;
-    if (session_status() == PHP_SESSION_NONE)
-                session_start();
-    $_SESSION["username"] = $row["username"];
-    $_SESSION["id"] = $row["id"];//NOVO
-    $_SESSION["fraseApresentacao"] = "Ola o meu nome é " . $row["nome"] . " e tenho " . $row["idade"] . " anos de idade";
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $found = true;
+        if(!isset($_SESSION))
+        session_start();
+        $_SESSION["username"] = $row["username"];
+        $_SESSION["id"] = $row["id"];//NOVO
+        $_SESSION["fraseApresentacao"] = " ". $row["nome"] ;
+    }
+    include './mysql/mysqlClose.php';
 }
-include './mysql/mysqlClose.php';
-
 include("index.php");
 
 ?>
